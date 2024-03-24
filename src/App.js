0e23dc876709eda56aef9ac57119d659067dcd13
preechawutp <157285890+preechawutp@users.noreply.github.com>
2024-03-24 09:33:28 +0700

    fix App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from './components/Main';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import ScheduleTable from './components/ScheduleTable';
-import ShowCourse  from './components/ShowCourse ';
+import ShowCourse from './components/ShowCourse';
 import Result from './components/Result';
 import TeacherTable from './components/Teachertable';
 
@@ -15,7 +15,7 @@ const App = () => {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/" element={<Main />} />
         <Route path="/table" element={<ScheduleTable />} />
-        <Route path="/course" element={<ShowCourse  />} />
+        <Route path="/course" element={<ShowCourse />} />
         <Route path="/result" element={<Result />} />
         <Route path="/teacher" element={<TeacherTable />} />
       </Routes>
