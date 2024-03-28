2e571f5a5f909bc78d17cc4b17b5f35bbc5115e8
preechawutp <157285890+preechawutp@users.noreply.github.com>
2024-03-28 19:12:26 +0700

    แก้บัคนิดหน่อย

--- src/components/Result.js
+++ src/components/Result.js
@@ -17,66 +17,66 @@ const Result = () => {
 
   useEffect(() => {
     const fetchCourses = async () => {
-        try {
-          const querySnapshot = await getDocs(collection(db, 'ChooseSubject'));
-          const coursesData = querySnapshot.docs.map(doc => doc.data());
-          setSearchedCourses(coursesData);
-        } catch (error) {
-          console.error('Error fetching courses: ', error);
-        }
-      };
+      try {
+        const querySnapshot = await getDocs(collection(db, 'ChooseSubject'));
+        const coursesData = querySnapshot.docs.map(doc => doc.data());
+        setSearchedCourses(coursesData);
+      } catch (error) {
+        console.error('Error fetching courses: ', error);
+      }
+    };
 
     fetchCourses();
   }, []);
 
   const queryCourses = async ({
-      teacher,
-      term,
-      year,
+    teacher,
+    term,
+    year,
   }) => {
-      const q = query(ChooseSubjectRef,
-          where('teacher', '==', teacher),
-          where('term', '==', term),
-          where('years', '==', year),
-          where('status', '==', 'active')
-      );
-      const querySnapshot = await getDocs(q);
-      // const coursesArray = querySnapshot.docs.map(doc => doc.data());
-      const coursesArray = querySnapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-      setSearchedCourses(coursesArray);
+    const q = query(ChooseSubjectRef,
+      where('teacher', '==', teacher),
+      where('term', '==', term),
+      where('years', '==', year),
+      where('status', '==', 'active')
+    );
+    const querySnapshot = await getDocs(q);
+    // const coursesArray = querySnapshot.docs.map(doc => doc.data());
+    const coursesArray = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    setSearchedCourses(coursesArray);
   };
 
 
   return (
-    <div> 
-        <Navbar/>
-    <div className='container'>
+    <div>
+      <Navbar />
+      <div className='container'>
         <div className="schedule-table-container mt-5">
-        <h2>ตารางสอน</h2>
-        <Dropdown queryCourses={queryCourses} />
-            <table className="table table-hover mt-3">
-                <thead class="thead-light">
-                    <tr>
-                    <th scope="col">ลำดับที่</th>
-                    <th scope="col">รหัสวิชา</th>
-                    <th scope="col">ชื่อวิชา</th>
-                    <th scope="col">หน่วยกิต</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {searchedCourse.map((searchedCourse, index) => (
-                      <tr key={index}>
-                          <td>{index + 1}</td>
-                          <td>{searchedCourse.code}</td>
-                          <td>{searchedCourse.name}</td>
-                          <td>{searchedCourse.credit}</td>
-                      </tr>
-                    ))}
-                </tbody>
-            </table>
+          <h2>ตารางสอน</h2>
+          <Dropdown queryCourses={queryCourses} />
+          <table className="table table-hover mt-3">
+            <thead className="thead-light">
+              <tr>
+                <th scope="col">ลำดับที่</th>
+                <th scope="col">รหัสวิชา</th>
+                <th scope="col">ชื่อวิชา</th>
+                <th scope="col">หน่วยกิต</th>
+              </tr>
+            </thead>
+            <tbody>
+              {searchedCourse.map((searchedCourse, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{searchedCourse.code}</td>
+                  <td>{searchedCourse.name}</td>
+                  <td>{searchedCourse.credit}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
